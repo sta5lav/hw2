ca049bc84b1dc868f696a66fbb18a47d3b81387e
Stanislav <stdu92@mail.ru>
2023-02-13 12:03:48 +0300

    Переменные

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+        public static void main(String[] args) {
+            task1();
+            task2();
+            task3();
+        }
+
+        public static void task1 () {
+            var dog = 8.0;
+            var cat = 3.6;
+            var paper = 763789;
+            System.out.println(dog + " " + cat + " " + paper);
+        }
+
+        public static void task2 () {
+            var dog = 8.0 + 4;
+            var cat = 3.6 + 4;
+            var paper = 763789 + 4;
+            System.out.println(dog + " " + cat + " " + paper);
+        }
+        public static void task3 () {
+            var dog = 8.0 - 3.5;
+            var cat = 3.6 - 1.6;
+            var paper = 763789 - 7639;
+            System.out.println(dog + " " + cat + " " + paper);
+        }
+    }
